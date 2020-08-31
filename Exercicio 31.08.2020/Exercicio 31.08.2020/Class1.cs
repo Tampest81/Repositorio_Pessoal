b0be44a69b1f92a446db1c48b8d9db2982140c23
Tampest81 <43623947+Tampest81@users.noreply.github.com>
2020-08-31 08:59:38 -0300

    Add files via upload

---
+++ Exercicio 31.08.2020/Exercicio 31.08.2020/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Program
+{
+
+	public class Class1
+	{
+		public float A;
+		public float B;
+		public float C;
+
+		public float Valor()
+        {
+			float p = (A + B + C) / 2;
+			float value = (p * (p - A) * (p - B) * (p - C));
+			return value;
+		}
+	}
+}
