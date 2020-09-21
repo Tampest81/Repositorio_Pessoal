e25482c3fd3e0afd2d3e25b646bf4460414b5162
Alessandro <maquexxx@gmail.com>
2020-09-21 11:11:38 -0300

    Organização

---
+++ Archive _ Test/Exercicio 14.09.2020.03/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Exercicio_31._08._2020._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Valores produto;
+
+            produto = new Valores();
+
+                Console.Write("Nome do Produto: ");
+                produto.nome = Console.ReadLine();
+
+                Console.Write("Preço do Produto: ");
+                produto.preco = double.Parse(Console.ReadLine());
+
+                Console.Write("Quantidade do Produto no Estoque: ");
+                produto.quantidade = int.Parse(Console.ReadLine());
+
+            produto.Espacamento();
+
+            Console.WriteLine("Dados do Produto: " + produto);    
+
+            produto.Espacamento();
+
+                Console.Write("Digite a quantidade para ser ADICIONADO no ESTOQUE: ");
+                int valorAdicional = int.Parse(Console.ReadLine());
+
+        produto.quantidade = valorAdicional + produto.quantidade;
+
+            produto.Espacamento();
+
+            Console.WriteLine("Dados do Produto Atualizados: " + produto);
+
+            produto.Espacamento();
+
+                Console.Write("Digite a quantidade para ser REMOVIDO no ESTOQUE: ");
+                valorAdicional = int.Parse(Console.ReadLine());
+
+        produto.quantidade = produto.quantidade - valorAdicional ;
+
+            produto.Espacamento();
+
+            Console.WriteLine("Dados do Produto Atualizados: " + produto);
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Archive _ Test/Exercicio 31.08.2020/Exercicio 31.08.2020/Program.cs
@@ -0,0 +1,41 @@
+﻿using Program;
+using System;
+using System.Security.Cryptography;
+
+namespace Exercicio_31._08._2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 a,b;
+
+            a = new Class1();
+            b = new Class1();
+
+            Console.WriteLine("Valores A");
+            a.A = float.Parse(Console.ReadLine());
+            a.B = float.Parse(Console.ReadLine());
+            a.C = float.Parse(Console.ReadLine());
+            Console.WriteLine("Valores B");
+            b.A = float.Parse(Console.ReadLine());
+            b.B = float.Parse(Console.ReadLine());
+            b.C = float.Parse(Console.ReadLine());
+
+            float AreaA = a.Valor();
+            float AreaB = b.Valor();
+
+            if (AreaA > AreaB)
+            {
+                Console.WriteLine("Area A Maior que Area B");
+            }
+            else if (AreaB > AreaA)
+            {
+                Console.WriteLine("Area B Maior que Area A");
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
