21ebf1a1f8394d93eef8497e28d67ea8e7bdbb0e
Alessandro <maquexxx@gmail.com>
2020-09-21 11:07:03 -0300

    .

---
+++ Exercícios/Exercício individual - Tema Aula 24.08.20/Exercicio 1 24.08.2020/Exercicio 1 24.08.2020/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Exercicio_1_24._08._2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Valor pessoa1, pessoa2;
+
+            pessoa1 = new Valor();
+            pessoa2 = new Valor();
+
+            float mediaSalarial;
+            float diferencial;
+
+            Console.Write("Nome1: ");
+                pessoa1.pessoa = Console.ReadLine();
+            Console.Write("Idade1: ");
+                pessoa1.idade = float.Parse(Console.ReadLine());
+            Console.Write("Salário1: ");
+                pessoa1.salario = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(" ");
+
+            Console.Write("Nome2: ");
+                pessoa2.pessoa = Console.ReadLine();
+            Console.Write("Idade2: ");
+                pessoa2.idade = float.Parse(Console.ReadLine());
+            Console.Write("Salário2: ");
+                pessoa2.salario = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(" ");
+
+            if (pessoa1.idade > pessoa2.idade)
+            {
+                diferencial = pessoa1.idade - pessoa2.idade;
+                Console.WriteLine(pessoa1.pessoa + " é mais velho que " + pessoa2.pessoa + " por " + diferencial + " anos.");
+            }
+            else
+            {
+                diferencial = pessoa2.idade - pessoa1.idade;
+                Console.WriteLine(pessoa2.pessoa + " é mais velho que " + pessoa1.pessoa + " por " + diferencial + " anos.");
+            }
+
+            Console.WriteLine(" ");
+
+            mediaSalarial = (pessoa1.salario + pessoa2.salario) / 2;
+            Console.WriteLine("A média salarial é : " + mediaSalarial);
+
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
---
+++ Exercícios/Exercício individual - Tema Aula 31.08.20/Exercicio 31.08.2020.02/Exercicio 31.08.2020.02/Class2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_31._08._2020._02
+{
+	public class Valores
+	{
+		public string nome;
+		public double preco;
+		public int quantidade;
+
+		public double ValorTotalEstoque()
+		{
+			double valorTotal = preco * quantidade;
+			return valorTotal;
+		}
+
+		public void Espacamento()
+		{
+			Console.Write("  ");
+			Console.WriteLine("  ");
+		}
+
+
+		public override string ToString()
+		{
+			return nome + "(Nome do produto) "
+				+ ", Preço Unico: $"
+				+ preco.ToString("F2", CultureInfo.InvariantCulture) + ", Quantidade em estoque:"
+				+ quantidade
+				+ ", Total em real: $"
+				+ ValorTotalEstoque().ToString("F2", CultureInfo.InvariantCulture);
+		}
+
+	}
+}
